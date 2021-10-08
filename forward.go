3fee04bb6fdce56aa5892488477718c64654e6b6
zengh1 <zh1105336755@gmail.com>
2021-10-08 15:08:49 +0800

    forward module complete

---
+++ forward.go
@@ -0,0 +1,23 @@
+package socket5
+
+import (
+	"io"
+	"net"
+)
+
+func forward(cliConn, targetConn net.Conn) (err error) {
+	fn := func (dst, src net.Conn) error {
+		_, err := io.Copy(dst, src)
+		return err
+	}
+
+	go func () {
+		err = fn(targetConn, cliConn)	// 将客户端数据发送到目的服务器
+	}()
+	
+	go func () {
+		err = fn(cliConn, targetConn)	// 将目的服务器响应发送给客户端
+	}()
+
+	return
+}
